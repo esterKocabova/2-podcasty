84eac17103c9f239b7a22185f643614dcb4ff727
Ester <kocabova@mamiart.cz>
2024-02-17 22:39:08 +0100

    oprava

--- 2-podcast/src/index.jsx
+++ 2-podcast/src/index.jsx
@@ -1,8 +1,24 @@
 import { createRoot } from 'react-dom/client';
 import { HomePage } from './pages/HomePage';
 import './global.css';
-import {Episode} from './components/episode/Episode'
+import  {episodes} from './episodes.js'
+
+const App = () => {
+ return(
+  <div className="episodes-list">
+  <div className="">{episodes.map((episode) => (
+      <div key={episode.num} className="episode"> 
+      <div className="episode__num">{episode.num}</div>
+      <div className="episode__body">
+          <div className="episode__title">{episode.title}</div>
+          <div className="episode__guest">{episode.guest}</div>
+      </div></div>
+      ))}
+  </div>
+  </div>
+ )
+}
 
 createRoot(
   document.querySelector('#app'),
-).render(<Episode />);
+).render(<App />);
